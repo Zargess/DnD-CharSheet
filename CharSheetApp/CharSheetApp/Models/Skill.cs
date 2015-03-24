3159614bad005425648ab53227b99a66de68f267
Zargess <haagh9@hotmail.com>
2015-03-24 23:07:19 +0100

    New app approach

---
+++ CharSheetApp/CharSheetApp/Models/Skill.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharSheetApp.Models {
+    public class Skill {
+        public string Name { get; private set; }
+        public bool Proficient { get; private set; }
+        public int Value { get; private set; }
+        private int _bonus;
+        private Stat _stat;
+
+        public Skill(Stat stat, string name, bool proficient, int bonus) {
+            _stat = stat;
+            _bonus = bonus;
+            Name = name;
+            Proficient = proficient;
+        }
+    }
+}
---
+++ CharSheetApp/CharSheetApp/Models/Stat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharSheetApp.Models {
+    public class Stat {
+        public string Name { get; private set; }
+        public int Score { get; private set; }
+        public int Modifier { get; private set; }
+
+        public Stat(string name, int score) {
+            Name = name;
+            Score = score;
+            Modifier = (int) (-5 + 0.5 * Score);
+        }
+    }
+}
